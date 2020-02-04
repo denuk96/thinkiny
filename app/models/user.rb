109c577f4f30670528de7ab9292fe732b85d39e2
Andrii <a.rozputnii@gmail.com>
2020-02-04 09:45:41 +0200

    need rails db:drop/create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,9 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id          :bigint           not null, primary key
-#  first_name  :string
-#  second_name :string
-#  admin       :boolean          default(FALSE)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#
-
 class User < ApplicationRecord
+  authenticates_with_sorcery!
+
   has_many :course_users
   has_many :courses, through: :course_users
   has_many :check_ins
   has_many :lessons, through: :check_ins
+
 end
