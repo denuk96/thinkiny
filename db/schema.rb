7aa6892b82d06d876f84d20e66c9fc984bfdfa75
Andrii <a.rozputnii@gmail.com>
2020-02-10 13:10:02 +0200

    log the activity&& protecting login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_205205) do
+ActiveRecord::Schema.define(version: 2020_02_10_102843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,8 +65,17 @@ ActiveRecord::Schema.define(version: 2020_02_05_205205) do
     t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
     t.string "last_name"
+    t.integer "failed_logins_count", default: 0
+    t.datetime "lock_expires_at"
+    t.string "unlock_token"
+    t.datetime "last_login_at"
+    t.datetime "last_logout_at"
+    t.datetime "last_activity_at"
+    t.string "last_login_from_ip_address"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at"
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["unlock_token"], name: "index_users_on_unlock_token"
   end
 
   add_foreign_key "check_ins", "lessons"
