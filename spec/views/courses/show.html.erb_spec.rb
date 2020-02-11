fb08e5cf701542363997bf9cd40d6340c73c7984
Denys Taradada <denis.taradada@gmail.com>
2020-02-11 20:21:39 +0200

    tests

---
+++ spec/views/courses/show.html.erb_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe 'courses/show', type: :view do
+  before(:each) do
+    assign(:course,
+        Course.create!(name: "test name", description:'description')
+    )
+  end
+  it 'displays course details correctly' do
+
+    render
+
+    expect(rendered).to match /test name/
+    expect(rendered).to match /description/
+    # rendered.should contain('test description')
+  end
+end
