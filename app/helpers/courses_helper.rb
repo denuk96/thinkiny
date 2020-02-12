b6bfbeffe2c48cbf61defaa2598eb80e704333a4
Denys Taradada <denis.taradada@gmail.com>
2020-02-12 18:23:36 +0200

    map displaying fixed, buttons hiden

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,18 @@
 module CoursesHelper
+  def verify_instructor(course, user)
+    organizer = course.course_users.find_by(role: 'organizer', user_id: user.id)&.user&.id
+    instructor = course.course_users.find_by(role: 'instructor', user_id: user.id)&.user&.id
+    true if  user.id == organizer ||
+             user.id == instructor ||
+             user.admin == true
+  end
+
+  def verify_organizer(course, user)
+    organizer = course.course_users.find_by(role: 'organizer', user_id: user.id)&.user&.id
+    true if user.id == organizer || user.admin == true
+  end
+
+  def already_joined?(course, user)
+    true if course.course_users.find_by(user_id: user.id).present?
+  end
 end
