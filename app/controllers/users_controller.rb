81bb9a139d481023099846b7c6d333173b2f4dd0
Andrii <a.rozputnii@gmail.com>
2020-02-16 18:47:09 +0200

    show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
   include CheckAuthorization
-  before_action :current_user_already_exist?, except: :show
-  before_action :set_user, only: %i[show edit update destroy]
-  before_action :admin_verify, only: %i[edit update destroy]
-  before_action :not_authenticated, only: :show
+  before_action :current_user_already_exist?, except: %i[ show edit update ]
+  before_action :set_user
+  before_action :not_authenticated, only: %i[show index edit update]
 
-  def index
-    redirect_to signup_path
-  end
+  def index; end
 
-  def show; end
+  def show;  end
 
   def new
     @user = User.new
@@ -19,18 +16,30 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to root_path, notice: 'Welcome aboard!'
+      redirect_to root_path, notice: 'Welcome!'
     else
       render :new
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User has been edited'
+    else
+      render :edit
+    end
+  end
+
   private
+
   def set_user
     @user = User.find(params[:id])
   end
+
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :crypted_password, :salt,
-                                 :first_name, :last_name)
+    params.require(:user).permit(:email, :password, :password_confirmation, :crypted_password,
+                                 :salt, :first_name, :last_name)
   end
 end
