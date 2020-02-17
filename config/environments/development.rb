c508c9298cb7e2799efbc77e8f057d8507d48c96
Vyacheslav <truesspay@gmail.com>
2020-02-17 22:57:13 +0200

    replace first position on form/map on user position

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "0c5a144e.ngrok.io"
   config.action_mailer.default_url_options = { host: 'localhost', port: '3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
