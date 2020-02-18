d52876fb925a61fef11567bbc97eadb559d6dc53
Vyacheslav <truesspay@gmail.com>
2020-02-19 00:17:19 +0200

    fix autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require("packs/geocomplete")
 import '../stylesheets/application'
 import './bootstrap_custom.js'
 
