110be24a404e3750cdd870ad797147ca48a9cef6
Andrii <a.rozputnii@gmail.com>
2020-02-18 17:50:31 +0200

    gooooogle

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -158,7 +158,7 @@ Rails.application.config.sorcery.configure do |config|
   config.google.user_info_mapping = {
     email: 'email',
     first_name: 'given_name',
-    last_name: 'family_name',
+    last_name: 'family_name'
     #picture: 'picture'
   }
   # For Microsoft Graph, the key will be your App ID, and the secret will be your app password/public key.
