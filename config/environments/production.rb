56e0b1d3815b3cfbdef67be55d83bc88fa26d179
Denys Taradada <denis.taradada@gmail.com>
2020-03-15 20:02:48 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: ENV['domain'] }
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
