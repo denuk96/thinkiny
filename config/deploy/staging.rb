8a3dfe5d45ac47dda8a61aa4ecf343f6574dd747
Denys Taradada <denis.taradada@gmail.com>
2020-03-15 12:21:40 +0200

    it does work!!!!!

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
-set :stage, "production"
+set :stage, "staging"
 set :branch, 'development'
 set :deploy_to, "/home/#{fetch(:user)}/apps/thinkiny_staging"
-set :rails_env, :staging
+set :rails_env, :production
 set :domain, "thinkiny-stage.pp.ua"
 set :puma_conf, "#{shared_path}/config/puma.rb"
