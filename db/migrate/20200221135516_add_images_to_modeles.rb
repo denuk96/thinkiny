08da103b31424fd2393433b1710efafb2b7099f3
Denys Taradada <denis.taradada@gmail.com>
2020-03-15 12:53:12 +0200

    try to fix migrations

--- db/migrate/20200221135516_add_images_to_modeles.rb
+++
@@ -1,8 +0,0 @@
-class AddImagesToModeles < ActiveRecord::Migration[6.0]
-  def change
-    add_column :courses, :pictures, :string, array: true, default: []
-    add_column :users, :picture, :string
-    add_column :lessons, :picture, :string
-
-  end
-end
