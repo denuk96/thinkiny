8d3b4067c0eaa816a6c0eb0bd25b38dca4a84f00
Andrii <a.rozputnii@gmail.com>
2020-03-15 15:57:39 +0200

    try 13

--- db/migrate/20200229144937_drop_active_storage.rb
+++ db/migrate/20200229144937_drop_active_storage.rb
@@ -1,7 +1,5 @@
 class DropActiveStorage < ActiveRecord::Migration[6.0]
   def change
-    drop_table :active_storage_attachments
-    drop_table :active_storage_blobs
     add_column :lessons, :picture, :string
     add_column :users, :picture, :string
     add_column :courses, :logo, :string
