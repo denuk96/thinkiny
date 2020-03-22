c219d99698ff13b74f3832b7584a788dfbe13126
Andrii <a.rozputnii@gmail.com>
2020-03-22 13:04:48 +0200

    destroy task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,13 @@
 class TasksController < ApplicationController
-
+  before_action :set_course, :set_lesson
   def create
-    @course = Course.find(params[:course_id])
-    @lesson = Lesson.find(params[:lesson_id])
     @task = @lesson.tasks.create(tasks_params)
+    redirect_to course_path(@course)
+  end
+
+  def destroy
+    @task = @lesson.tasks.find(params[:id])
+    @task.destroy
     redirect_to course_path(@course)
   end
 
@@ -13,4 +17,11 @@ class TasksController < ApplicationController
     params.require(:task).permit(:title, :body)
   end
 
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
+  def set_lesson
+    @lesson = Lesson.find(params[:lesson_id])
+  end
 end
