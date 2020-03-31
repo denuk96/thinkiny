e12f5b55c3529085eaf4e24221beae53fd914726
Denys <denys.taradada.triare@gmail.com>
2020-03-31 18:28:47 +0300

    fullcalendar installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,22 +3,37 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-// require("@rails/activestorage").start()
-require("channels")
-require("jquery")
-require("packs/geocomplete")
-require("packs/gmaps")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("channels");
+require("jquery");
+require("packs/geocomplete");
+require("packs/gmaps");
+
 import "@fortawesome/fontawesome-free/js/all";
 import '../stylesheets/application'
 import './bootstrap_custom.js'
 
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// calendar goes here
+
+import * as Calendar from "./fullcalendar/core/main";
+import  dayGridPlugin from "./fullcalendar/daygrid/main";
+import interactionPlugin from "./fullcalendar/interaction/main";
+import timeGridPlugin from "./fullcalendar/timegrid/main.min";
+import listPlugin from './fullcalendar/list/main';
+
+import "./fullcalendar/core/main.css";
+import "./fullcalendar/daygrid/main.css";
+import "./fullcalendar/timegrid/main.css";
+import './fullcalendar/list/main.css';
+
+window.FullCalendar = Calendar;
+window.dayGridPlugin = dayGridPlugin;
+window.interactionPlugin = interactionPlugin;
+window.timeGridPlugin = timeGridPlugin;
+window.listPlugin = listPlugin;
+
+// calendar ends here
+
 Notification.requestPermission().then(function (result) {})
