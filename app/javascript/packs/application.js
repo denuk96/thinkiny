fe0f4565666e6b9a81a04db09769895910663b2c
Denys <denys.taradada.triare@gmail.com>
2020-04-01 17:13:24 +0300

    try to fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start();
-require("turbolinks").start();
+// require("turbolinks").start();
 require("channels");
 require("jquery");
 require("packs/geocomplete");
@@ -14,14 +14,7 @@ import "@fortawesome/fontawesome-free/js/all";
 import '../stylesheets/application'
 import './bootstrap_custom.js'
 
-
 // calendar goes here
-
-// import * as Calendar from "./fullcalendar/core/main";
-// import  dayGridPlugin from "./fullcalendar/daygrid/main";
-// import interactionPlugin from "./fullcalendar/interaction/main";
-// import timeGridPlugin from "./fullcalendar/timegrid/main";
-// import listPlugin from './fullcalendar/list/main';
 
 import * as Calendar from "@fullcalendar/core";
 import  dayGridPlugin from "@fullcalendar/daygrid";
@@ -32,19 +25,12 @@ import bootstrapPlugin from '@fullcalendar/bootstrap';
 import timelinePlugin from '@fullcalendar/timeline';
 
 
-
-
 import '@fullcalendar/core/main.css';
 import "@fullcalendar/core/main.css";
 import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
 import '@fullcalendar/list/main.css';
 import '@fullcalendar/timeline/main.css';
-
-// import "./fullcalendar/core/main.css";
-// import "./fullcalendar/daygrid/main.css";
-// import "./fullcalendar/timegrid/main.css";
-// import './fullcalendar/list/main.css';
 
 window.FullCalendar = Calendar;
 window.dayGridPlugin = dayGridPlugin;
